9da38035892191b8ccd2bef8577aaae73bb4d6d8
Derpet1 <aspancuk39@gmail.com>
2025-04-08 10:08:12 +0300

    up1

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+exports.myTask = (callback) => {
+    console.log('Task 1: Hello Spanchuk!');
+    callback();
+  };
+  
+  exports.myTask2 = async () => {
+    console.log('Task 2: Hello Spanchuk!');
+  };
+  
+  exports.myTask3 = async () => {
+    console.log('Task 3: Hello Spanchuk!');
+  };
+  
+  exports.myTask4 = async () => {
+    console.log('Task 4: Hello Spanchuk!');
+  };
+  
