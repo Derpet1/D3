8d0ff86e30a829174b0afc5e09ee394a2aedae49
Derpet1 <aspancuk39@gmail.com>
2025-04-09 10:00:31 +0300

    up2

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,26 @@
-exports.myTask = (callback) => {
-    console.log('Task 1: Hello Spanchuk!');
-    callback();
-  };
-  
-  exports.myTask2 = async () => {
-    console.log('Task 2: Hello Spanchuk!');
-  };
-  
-  exports.myTask3 = async () => {
-    console.log('Task 3: Hello Spanchuk!');
-  };
-  
-  exports.myTask4 = async () => {
-    console.log('Task 4: Hello Spanchuk!');
-  };
-  
+
+// exports.myTask = (callback) => {
+//   console.log('Task 1: Hello Spanchuk!');
+//   callback();
+// };
+
+// exports.myTask2 = async () => {
+//   console.log('Task 2: Hello Spanchuk!');
+// };
+
+// exports.myTask3 = async () => {
+//   console.log('Task 3: Hello Spanchuk!');
+// };
+
+// exports.myTask4 = async () => {
+//   console.log('Task 4: Hello Spanchuk!');
+// };
+
+
+const gulp = require('gulp');
+
+exports.cloneFiles = async () => {
+  console.log('Копіювання HTML файлів з app/ в public...');
+  return gulp.src('app/*.html')
+    .pipe(gulp.dest('public'));
+};
